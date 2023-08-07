2edf9bdef9e12e61cff7a2c7c55921bc04d2ff10
Toper <86833913+Toperlock@users.noreply.github.com>
2023-08-07 14:16:38 +0800

    Update dd_daily.js

--- task/dd_daily.js
+++ task/dd_daily.js
@@ -1,85 +1,38 @@
 /*
  * 钉钉打卡提醒
- * @author: yichahucha
+ * @author: @yichahucha
+ * @thanks: @Peng-YM
  * 更新地址：https://raw.githubusercontent.com/yichahucha/surge/master/clock_in.js
  * 修改内容：去除有道每日一言
 */
-const $tool = new Tool()
-$tool.get('', function (error, response, data) {
-    let obj = JSON.parse(data);
-    let date = new Date();
-    let isAM = date.getHours() < 12 ? true : false;
-    let title = '钉钉打卡' + (isAM ? ' in' : ' out') + (isAM ? ' ☀️' : ' 🌙');
-    let subtitle = '';
-    let scheme = 'dingtalk://dingtalkclient/page/link?url=https://attend.dingtalk.com/attend/index.html';
-    let content = "不要忘记打卡了！";
-    let option = {"open-url" : scheme};
-    if (!error) {
+const $ = API("钉钉打卡");
+
+$.http
+    .get({
+        url: 'https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2',
+    })
+    .then((response) => {
+        let obj = JSON.parse(response.body);
+        let date = new Date();
+        let isAM = date.getHours() < 12 ? true : false;
+        let title = '钉钉打卡' + (isAM ? ' in' : ' out') + (isAM ? ' ☀️' : ' 🌙');
+        let subtitle = '';
+        let scheme = 'dingtalk://dingtalkclient/page/link?url=https://attend.dingtalk.com/attend/index.html';
+        let content = "不要忘记打卡了！";
+        let option = {"open-url" : scheme};
+        /*
         if (obj && obj.length > 1) {
             let yi = obj[1];
             content = yi.title + '\n' + yi.summary + '\n\n' + content;
             option["media-url"] = yi.image[0];
         }
-    }
-    $tool.notify(title, subtitle, content, option);
-    $done();
-})
+        */
+        $.notify(title, subtitle, content, option);
+        $.done()
+    })
 
-function Tool() {
-    _node = (() => {
-        if (typeof require == "function") {
-            const request = require('request')
-            return ({ request })
-        } else {
-            return (null)
-        }
-    })()
-    _isLoon = typeof $loon !== "undefined";
-    _isSurge = typeof $httpClient != "undefined" && !_isLoon;
-    _isQuanX = typeof $task != "undefined"
-    this.isSurge = _isSurge
-    this.isQuanX = _isQuanX
-    this.isResponse = typeof $response != "undefined"
-    this.notify = (title, subtitle, message, option) => {
-        if (_isQuanX) $notify(title, subtitle, message, option)
-        if (_isSurge) $notification.post(title, subtitle, message, {"url":option["open-url"]})
-        if (_isLoon) $notification.post(title, subtitle, message, option["open-url"])
-        if (_node) console.log(JSON.stringify({ title, subtitle, message }));
-    }
-    this.write = (value, key) => {
-        if (_isQuanX) return $prefs.setValueForKey(value, key)
-        if (_isSurge) return $persistentStore.write(value, key)
-    }
-    this.read = (key) => {
-        if (_isQuanX) return $prefs.valueForKey(key)
-        if (_isSurge) return $persistentStore.read(key)
-    }
-    this.get = (options, callback) => {
-        if (_isQuanX) {
-            if (typeof options == "string") options = { url: options }
-            options["method"] = "GET"
-            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-        }
-        if (_isSurge) $httpClient.get(options, (error, response, body) => { callback(error, _status(response), body) })
-        if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
-    }
-    this.post = (options, callback) => {
-        if (_isQuanX) {
-            if (typeof options == "string") options = { url: options }
-            options["method"] = "POST"
-            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-        }
-        if (_isSurge) $httpClient.post(options, (error, response, body) => { callback(error, _status(response), body) })
-        if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
-    }
-    _status = (response) => {
-        if (response) {
-            if (response.status) {
-                response["statusCode"] = response.status
-            } else if (response.statusCode) {
-                response["status"] = response.statusCode
-            }
-        }
-        return response
-    }
-}
+
+// prettier-ignore: https://github.com/Peng-YM/QuanX/blob/master/Tools/OpenAPI/README.md
+/*********************************** API *************************************/
+function ENV(){const e="undefined"!=typeof $task,t="undefined"!=typeof $loon,s="undefined"!=typeof $httpClient&&!t,i="function"==typeof require&&"undefined"!=typeof $jsbox;return{isQX:e,isLoon:t,isSurge:s,isNode:"function"==typeof require&&!i,isJSBox:i,isRequest:"undefined"!=typeof $request,isScriptable:"undefined"!=typeof importModule}}function HTTP(e={baseURL:""}){const{isQX:t,isLoon:s,isSurge:i,isScriptable:n,isNode:o}=ENV(),r=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/;const u={};return["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].forEach(l=>u[l.toLowerCase()]=(u=>(function(u,l){l="string"==typeof l?{url:l}:l;const h=e.baseURL;h&&!r.test(l.url||"")&&(l.url=h?h+l.url:l.url);const a=(l={...e,...l}).timeout,c={onRequest:()=>{},onResponse:e=>e,onTimeout:()=>{},...l.events};let f,d;if(c.onRequest(u,l),t)f=$task.fetch({method:u,...l});else if(s||i||o)f=new Promise((e,t)=>{(o?require("request"):$httpClient)[u.toLowerCase()](l,(s,i,n)=>{s?t(s):e({statusCode:i.status||i.statusCode,headers:i.headers,body:n})})});else if(n){const e=new Request(l.url);e.method=u,e.headers=l.headers,e.body=l.body,f=new Promise((t,s)=>{e.loadString().then(s=>{t({statusCode:e.response.statusCode,headers:e.response.headers,body:s})}).catch(e=>s(e))})}const p=a?new Promise((e,t)=>{d=setTimeout(()=>(c.onTimeout(),t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)),a)}):null;return(p?Promise.race([p,f]).then(e=>(clearTimeout(d),e)):f).then(e=>c.onResponse(e))})(l,u))),u}function API(e="untitled",t=!1){const{isQX:s,isLoon:i,isSurge:n,isNode:o,isJSBox:r,isScriptable:u}=ENV();return new class{constructor(e,t){this.name=e,this.debug=t,this.http=HTTP(),this.env=ENV(),this.node=(()=>{if(o){return{fs:require("fs")}}return null})(),this.initCache();Promise.prototype.delay=function(e){return this.then(function(t){return((e,t)=>new Promise(function(s){setTimeout(s.bind(null,t),e)}))(e,t)})}}initCache(){if(s&&(this.cache=JSON.parse($prefs.valueForKey(this.name)||"{}")),(i||n)&&(this.cache=JSON.parse($persistentStore.read(this.name)||"{}")),o){let e="root.json";this.node.fs.existsSync(e)||this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.root={},e=`${this.name}.json`,this.node.fs.existsSync(e)?this.cache=JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)):(this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.cache={})}}persistCache(){const e=JSON.stringify(this.cache,null,2);s&&$prefs.setValueForKey(e,this.name),(i||n)&&$persistentStore.write(e,this.name),o&&(this.node.fs.writeFileSync(`${this.name}.json`,e,{flag:"w"},e=>console.log(e)),this.node.fs.writeFileSync("root.json",JSON.stringify(this.root,null,2),{flag:"w"},e=>console.log(e)))}write(e,t){if(this.log(`SET ${t}`),-1!==t.indexOf("#")){if(t=t.substr(1),n||i)return $persistentStore.write(e,t);if(s)return $prefs.setValueForKey(e,t);o&&(this.root[t]=e)}else this.cache[t]=e;this.persistCache()}read(e){return this.log(`READ ${e}`),-1===e.indexOf("#")?this.cache[e]:(e=e.substr(1),n||i?$persistentStore.read(e):s?$prefs.valueForKey(e):o?this.root[e]:void 0)}delete(e){if(this.log(`DELETE ${e}`),-1!==e.indexOf("#")){if(e=e.substr(1),n||i)return $persistentStore.write(null,e);if(s)return $prefs.removeValueForKey(e);o&&delete this.root[e]}else delete this.cache[e];this.persistCache()}notify(e,t="",l="",h={}){const a=h["open-url"],c=h["media-url"];if(s&&$notify(e,t,l,h),n&&$notification.post(e,t,l+`${c?"\n多媒体:"+c:""}`,{url:a}),i){let s={};a&&(s.openUrl=a),c&&(s.mediaUrl=c),"{}"===JSON.stringify(s)?$notification.post(e,t,l):$notification.post(e,t,l,s)}if(o||u){const s=l+(a?`\n点击跳转: ${a}`:"")+(c?`\n多媒体: ${c}`:"");if(r){require("push").schedule({title:e,body:(t?t+"\n":"")+s})}else console.log(`${e}\n${t}\n${s}\n\n`)}}log(e){this.debug&&console.log(`[${this.name}] LOG: ${this.stringify(e)}`)}info(e){console.log(`[${this.name}] INFO: ${this.stringify(e)}`)}error(e){console.log(`[${this.name}] ERROR: ${this.stringify(e)}`)}wait(e){return new Promise(t=>setTimeout(t,e))}done(e={}){s||i||n?$done(e):o&&!r&&"undefined"!=typeof $context&&($context.headers=e.headers,$context.statusCode=e.statusCode,$context.body=e.body)}stringify(e){if("string"==typeof e||e instanceof String)return e;try{return JSON.stringify(e,null,2)}catch(e){return"[object Object]"}}}(e,t)}
+/*****************************************************************************/
